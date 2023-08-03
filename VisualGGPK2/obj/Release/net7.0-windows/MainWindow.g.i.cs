d8678376c9489807423ad50c5745da2dd68a1aaf
vmv <17906882+vmv@users.noreply.github.com>
2023-08-03 12:49:40 +0300

    clean up_2

--- VisualGGPK2/obj/Release/net7.0-windows/MainWindow.g.i.cs
+++ VisualGGPK2/obj/Release/net7.0-windows/MainWindow.g.i.cs
@@ -311,7 +311,7 @@ namespace VisualGGPK2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VisualGGPK2;V0.16.1.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VisualGGPK2;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
